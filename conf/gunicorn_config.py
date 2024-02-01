682ff31c3c1d9e58c12cab020df331f3a55c65b4
jp <joaoooh@gmail.com>
2024-02-01 15:36:45 -0300

    Initial commit

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,8 @@
+# path gunicorn exe
+command = '/home/jpmol/projetos/django_projects/disclone/disclone_env/bin/gunicorn'
+# path app
+pythonpath = '/home/jpmol/projetos/django_projects/disclone/disclone_app'
+# where gunicorn listen
+bind = '127.0.0.1:8000'
+# numero de workers
+workers = 3
